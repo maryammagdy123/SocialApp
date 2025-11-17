73cdf99861c81e2bd7754ade4baaba4cb41db772
Maryam Magdy <144924477+maryammagdy123@users.noreply.github.com>
2025-11-17 20:41:22 +0200

    Integrate React Query by adding QueryClient and QueryClientProvider in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // importing font awesome library
-import'@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 // importing flowbite library
 import 'flowbite'
 import './index.css'
 import App from './App.jsx'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+
+    </QueryClientProvider>
   </StrictMode>,
 )
