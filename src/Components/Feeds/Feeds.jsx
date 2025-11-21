9e023f463cfbb59c277d399a5fde969fd06fa12a
Maryam Magdy <144924477+maryammagdy123@users.noreply.github.com>
2025-11-21 22:21:05 +0200

    Refactor Feeds component to improve error handling and add EmptyPosts component for no posts scenario

--- src/Components/Feeds/Feeds.jsx
+++ src/Components/Feeds/Feeds.jsx
@@ -1,18 +1,36 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 import Post from '../Post/Post';
 import LoadingSkeleton from '../LoadingSketlon/LoadingSketlon';
 import CreatePost from '../CreatePost/CreatePost';
 import { useQuery } from '@tanstack/react-query';
 import { getPosts } from '../../services/apiServices';
+import EmptyPosts from '../MyPosts/EmptyPosts';
 
 export default function Feeds() {
-	const { data: posts, isError, isLoading } = useQuery({
+	const { data: posts, isError, isLoading, error } = useQuery({
 		queryFn: getPosts,
 		queryKey: ['posts'],
 		refetchOnWindowFocus: true,
 	})
 
+
+	const renderPost = () => {
+		if (isLoading) return <LoadingSkeleton />;
+
+		if (isError)
+			return (
+				<div className="text-red-600 text-center bg-red-100 px-4 py-2 rounded-md">
+					{error?.message || 'Something went wrong.'}
+				</div>
+			);
+
+		if (posts?.length > 0)
+			return posts.map((post) => (
+				<Post key={post._id} post={post} showAllComments={false} />
+			));
+
+		return <EmptyPosts />;
+	};
 
 	return (
 		<div className="min-h-screen w-full lg:w-[80%] py-8 px-3 flex flex-col items-center mt-12 lg:ml-56 mx-auto">
@@ -23,23 +41,7 @@ export default function Feeds() {
 			<div className="w-full   max-w-2xl space-y-6">
 				<CreatePost />
 
-				{isLoading ? (
-					<LoadingSkeleton />
-				) : isError ? (
-					<div className="text-red-600 text-center bg-red-100 px-4 py-2 rounded-md">
-						{isError}
-					</div>
-				) : posts.length > 0 ? (
-					posts.map((post) => (
-						<Post key={post._id} post={post} showAllComments={false} />
-
-					))
-				) : (
-					<div className="text-center mt-10 text-gray-600">
-						<p className="text-lg font-medium">No posts available right now.</p>
-						<p className="text-sm text-gray-400">Check back later or try refreshing the page.</p>
-					</div>
-				)}
+				{renderPost()}
 			</div>
 		</div>
 
---
+++ src/Components/MyPosts/EmptyPosts.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function EmptyPosts() {
+	return (
+		<div className="text-center mt-10 text-gray-600">
+			<p className="text-lg font-medium">No posts available right now.</p>
+			<p className="text-sm text-gray-400">Check back later or try refreshing the page.</p>
+		</div>
+	)
+}
